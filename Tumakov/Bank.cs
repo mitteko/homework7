ca0fd37f1a952c4c36baf9f8ad4a8540ca8463da
Ulyana Novoselova <uliana.a.novoselova@gmail.com>
2024-12-12 15:40:04 +0300

    Добавьте файлы проекта.

---
+++ Tumakov/Bank.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumakov
+{
+    public enum accountType
+    {
+        cберегательный,
+        текущий
+    }
+    
+    /// <summary>
+    /// НОВЫЙ МЕТОД В САМОМ НИЗУ
+    /// </summary>
+    
+    internal class Bank
+    {
+
+        private uint id;
+        private double balance;
+        private accountType isaccType;
+        private static uint idgen = 0;
+
+
+        public Bank() { }
+
+        public Bank(uint id, double balance, accountType isaccType)
+        {
+            this.id = id;
+            this.balance = balance;
+            this.isaccType = isaccType;
+        }
+
+        //конструктор для генерируемого ID
+        public Bank(double balance, accountType isaccType)
+        {
+            this.id = IdForGen();
+            this.balance = balance;
+            this.isaccType = isaccType;
+        }
+
+        //новое значение ID
+        public void Information(uint isid)
+        {
+            this.id = isid;
+
+        }
+
+        public void VvodInformation(double balance, accountType istype)
+        {
+            this.balance = balance;
+        }
+
+        public void InformationForGen(uint isid)
+        {
+            this.id = IdForGen();
+
+        }
+
+        private uint IdForGen()
+        {
+            idgen++;
+            return idgen;
+        }
+
+        public void Input(double summa)
+        {
+            if (summa < balance)
+            {
+                this.balance -= summa;
+                Console.WriteLine("Деньги успешно сняты!");
+            }
+            else
+            {
+                Console.WriteLine("На счете недостаточно средств");
+            }
+        }
+
+        public void Output(double summa)
+        {
+            this.balance += summa;
+            Console.WriteLine("Счет успешно пополнен!");
+        }
+        
+        public void Print()
+        {
+            Console.WriteLine($"Номер счета: {id}\nБаланс: {balance}\nТип банковского счета: {isaccType}");
+        }
+
+
+        /// <summary>
+        /// 8.1 - НОВЫЙ МЕТОД.
+        /// В класс банковский счет, созданный в упражнениях 7.1- 7.3 добавить метод, который переводит деньги с одного счета на другой.
+        /// У метода два параметра: ссылка на объект класса банковский счет откуда снимаются деньги, второй параметр – сумма.
+        /// </summary>
+
+        public void Transfer(Bank targetAcc, double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Сумма перевода должна быть положительной");
+                return;
+            }
+
+            if (amount > this.balance)
+            {
+                Console.WriteLine("Недостаточно средств для перевода");
+                return;
+            }
+
+            this.Input(amount); //снимаем деньги с текущего счёта
+            targetAcc.Output(amount); //переводим средства на целевой счёт
+            Console.WriteLine($"Переведено {amount} на счёт {targetAcc.id}");
+        }
+    }
+
+}
---
+++ Tumakov/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumakov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Console.ReadKey();
+        }
+
+
+        /// Упражнение 8.2 
+        /// Реализовать метод, который в качестве входного параметра принимает
+        /// строку string, возвращает строку типа string, буквы в которой идут в обратном порядке. Протестировать метод.
+        static void Task1()
+        {
+            Console.WriteLine("Упражнение 8.2:");
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+            string reversed = ReversedString(input);
+
+            Console.WriteLine($"Строка '{input}' преобразована в '{reversed}'");
+        }
+        
+        //метод для разворачивания строки
+        static string ReversedString(string str)
+        {
+            char[] charsInput = str.ToCharArray();
+            Array.Reverse(charsInput);
+            return new string(charsInput);
+        }
+
+
+        /// Упражнение 8.3 Написать программу, которая спрашивает у пользователя имя файла. 
+        /// Если такого файла не существует, то программа выдает пользователю сообщение и заканчивает работу, 
+        /// иначе в выходной файл записывается содержимое исходного файла, но заглавными буквами.
+        static void Task2()
+        {
+            Console.WriteLine("Упражнение 8.3:");
+            Console.Write("Введите имя файла: ");
+            string inputFile = Console.ReadLine();
+
+            if (!File.Exists(inputFile))
+            {
+                Console.WriteLine("Такого файла не сущетсвует!!!");
+                return; 
+            }
+
+            string outputFile = Path.ChangeExtension(inputFile, ".out"); // - выходной файл
+
+            try
+            {
+                string message = File.ReadAllText(inputFile);
+                string upperMessage = message.ToUpper();
+
+                File.WriteAllText(outputFile, upperMessage);
+                Console.WriteLine($"Содержимое было успешно записано в файл: {outputFile}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+
+
+        /// Упражнение 8.4 
+        /// Реализовать метод, который проверяет реализует ли входной параметр метода интерфейс System.IFormattable. 
+        /// Использовать оператор is и as. (Интерфейс IFormattable обеспечивает функциональные возможности форматирования значения объекта в строковое представление.)
+        static void Task3()
+        {
+            Console.WriteLine("Упражнение 8.4:");
+            Console.Write("Введите ЧИСЛО: ");
+            int number = int.Parse(Console.ReadLine());
+            DateTime dateTime = DateTime.Now;
+            Console.Write("Введите СТРОКУ: ");
+            string text = Console.ReadLine();
+
+            CheckIFormattable(number);
+            CheckIFormattable(dateTime);
+            CheckIFormattable(text);
+        }
+
+        static void CheckIFormattable(object obj)
+        {
+            //если obj является экземпляром интерфейса IFormattable, производится явное преобразование этого объекта к типу IFormattable
+            if (obj is IFormattable formattable)
+            {
+                Console.WriteLine($"Объект {obj} реализует интерфейс IFormattable");
+            }
+            else
+            {
+                Console.WriteLine($"Объект {obj} не реализует интерфейс IFormattable");
+            }
+        }
+    }
+}
